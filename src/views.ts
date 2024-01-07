5998d9b37e5517566d6af26274f7e3248dff20a5
Sergey Kolesnik <sergey.s.kolesnik@gmail.com>
2024-01-07 05:18:23 +0300

    feat(views): columns view, box view, gallery view

--- src/views.ts
+++ src/views.ts
@@ -1,6 +1,10 @@
 import { provideStyle } from './utils'
 
 import tabularViewStyle from './css/tabular_view.css?inline'
+import boxViewStyle from './css/box_view.css?inline'
+import columnsViewStyle from './css/columns_view.css?inline'
+import galleryViewStyle from './css/gallery_view.css?inline'
+
 import hideDotRefs_WrapStyle from './css/hide_dot_refs__wrap.css?inline'
 import hideDotRefs_HoverStyle from './css/hide_dot_refs__hover.css?inline'
 
@@ -25,6 +29,39 @@ export function tabularView(toggle: boolean) {
     const key = 'tabular-view'
     if (toggle)
         provideStyle(key, tabularViewStyle)
+    else
+        provideStyle(key)
+}
+
+/**
+ * CSS: Box view
+ */
+export function boxView(toggle: boolean) {
+    const key = 'box-view'
+    if (toggle)
+        provideStyle(key, boxViewStyle)
+    else
+        provideStyle(key)
+}
+
+/**
+ * CSS: Columns view
+ */
+export function columnsView(toggle: boolean) {
+    const key = 'columns-view'
+    if (toggle)
+        provideStyle(key, columnsViewStyle)
+    else
+        provideStyle(key)
+}
+
+/**
+ * CSS: Gallery view
+ */
+export function galleryView(toggle: boolean) {
+    const key = 'gallery-view'
+    if (toggle)
+        provideStyle(key, galleryViewStyle)
     else
         provideStyle(key)
 }
